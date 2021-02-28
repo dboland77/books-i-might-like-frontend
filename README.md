dfe525844f59ea9d17c7b85904c3226c419906fb
David Boland <dboland77@gmail.com>
2021-02-28 14:01:30 +0000

    updated readme

--- README.md
+++ README.md
@@ -2,5 +2,15 @@
 
 Built with react - connects to the perlego-backend (Express API).
 All components styled with  css.
+CSS grid used for responsive layout
 Native fetch + promises uses for API communication
-Cypress used for end to end testing
+Functional components with React Hooks
+Cypress used for end to end testing (performed after prototyping)
+
+# Approach
+1. Create protototype components
+1. Style with css
+1. Load data
+1. Manually test
+1. Set up Cypress
+1. Test
--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from "react";
 
 function App() {
+
+
+  useEffect(async () => {
+    try{
+      const response = await fetch(j);
+      const data = await response.json();
+      console.log({data});
+    }
+  },[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        Hello
     </div>
   );
 }
